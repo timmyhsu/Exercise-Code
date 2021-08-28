e83061cc7768c3e63619bb5cd5140b3b543099b9
LS <43104666+timmyhsu@users.noreply.github.com>
2021-08-28 20:01:37 +0800

    Update a350.cpp

--- a350.cpp
+++ a350.cpp
@@ -1,41 +1,47 @@
-#include <iostream>
-
-using namespace std ;
-
-
-
-int main()
-{
-	int acc[9] = {8 ,16,24,32,40,48,56,64,80} ;
-	int mxlnth ;
-	int arr[9];
-	int ans = 1 ;
-	while( cin >> mxlnth )
-	{
-		ans = 1 ;
-		for (int x = 0 ; x < 9 ; x++)
-			cin >> arr[x] ;
-		int left = mxlnth ;
-		for (int x = 0 ; x < 9 ;x++ )
-		{
-			if (!arr[x]) continue ;
-			if (acc[x] > mxlnth ) 
-			{	
-				ans = -1 ;break ;
-			}
-			while(arr[x] != 0) 
-			{
-				if(left >= acc[x])
-				{
-					left -= acc[x] ; --arr[x] ;
-				}
-				else 
-				{
-					ans++ ;left = mxlnth ;
-				}
-			}
-		}
-		if(ans == -1) cout << "NO SOLUTION!!" << endl; 
-		else cout << ans << endl ;
-	}
+#include <iostream> 
+ 
+using namespace std ; 
+ 
+ 
+ 
+int main() 
+{ 
+        int acc[9] = {8 ,16,24,32,40,48,56,64,80} ; 
+        int mxlnth ; 
+        int arr[9]; 
+        int ans = 1 ; 
+        while( cin >> mxlnth ) 
+        { 
+                ans = 1 ; 
+                for (int x = 0 ; x < 9 ; x++) 
+                        cin >> arr[x] ; 
+                int left = mxlnth ; 
+                for(auto x =0 ;x<9;x++) 
+                { 
+                        if( mxlnth < acc[x]) 
+                        { 
+                                ans = -1 ;break ; 
+                        } 
+                } 
+                for (int x = 8 ; x >= 0 && ans != -1 ; --x ) 
+                { 
+                        //while(arr[x] != 0)  
+                        { 
+                                for (int y = x; y >=0 && arr[y]!=0 ;--y) 
+                                {        
+                                        while(left >= acc[y]) 
+                                        { 
+                                                left -= acc[y] ; --arr[y] ; 
+                                        } 
+                                        if(left < acc[1]) 
+                                        { 
+                                                ans++ ;left = mxlnth ;break ; 
+                                        } 
+ 
+                                } 
+                        } 
+                } 
+                if(ans == -1) cout << "NO SOLUTION!!" << endl;  
+                else cout << ans << endl ; 
+        } 
 }
